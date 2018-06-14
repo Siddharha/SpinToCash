cd28eb9fccef19a96ee1fb54425925ccb8f5ce35
Siddhartha Maji <siddhartha.ee.13@gmail.com>
2018-06-14 10:21:01 +0530

    Initial commit

---
+++ app/src/main/java/com/spintocash/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.spintocash;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        showSplash();
+    }
+
+    private void showSplash() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 5s = 5000ms
+                startActivity(new Intent(getBaseContext(),LogInActivity.class));
+            }
+        }, 5000);
+    }
+}
